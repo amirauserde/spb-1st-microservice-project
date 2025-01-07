8188b633f59cce91fda99ed2938c815a3ef20a82
amir <am.sharafi@gmail.com>
2025-01-07 22:31:20 +0330

    version 0.5 project run successful

--- order-microservice/src/test/java/com/amir/microservices/order_service/OrderServiceApplicationTests.java
+++ order-microservice/src/test/java/com/amir/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,18 +1,27 @@
 package com.amir.microservices.order_service;
 
+import com.amir.microservices.order_service.event.OrderPlacedEvent;
 import com.amir.microservices.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.MySQLContainer;
 
+import java.util.concurrent.CompletableFuture;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,11 +32,22 @@ class OrderServiceApplicationTests {
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 	@LocalServerPort
 	private Integer port;
+	// Mock KafkaTemplate
+	@MockitoBean
+	private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
 	@BeforeEach
 	void setup() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
+
+		// Mock a ListenableFuture for SendResult<String, OrderPlacedEvent>
+		// Suppress the unchecked warning for ListenableFuture
+		@SuppressWarnings("unchecked")
+		CompletableFuture<SendResult<String, OrderPlacedEvent>> future = Mockito.mock(CompletableFuture.class);
+
+		// Stub KafkaTemplate.send to return the mock ListenableFuture
+		doReturn(future).when(kafkaTemplate).send(anyString(), any(OrderPlacedEvent.class));
 	}
 
 	static {
@@ -41,7 +61,12 @@ class OrderServiceApplicationTests {
                 {
                      "skuCode": "galaxy_24",
                      "price": 700,
-                     "quantity": 1
+                     "quantity": 1,
+                     "userDetails": {
+				             "email": "am.sharafi@gmail.com",
+				             "firstName": "Amir",
+				             "lastName": "Sharafi"
+				             }
                 }
                 """;
 		InventoryClientStub.stubInventoryCall("galaxy_24", 1);
@@ -58,6 +83,10 @@ class OrderServiceApplicationTests {
 				.body().asString();
 
 		assertThat(responseStringBody, Matchers.is("Order placed successfully"));
+
+		// Verify that the KafkaTemplate's send method was called once
+//		verify(kafkaTemplate, times(1))
+//				.send(eq("order-placed"), any(OrderPlacedEvent.class));
 	}
 
 }
--- order-microservice/src/test/java/com/amir/microservices/order_service/stubs/InventoryClientStub.java
+++ order-microservice/src/test/java/com/amir/microservices/order_service/stubs/InventoryClientStub.java
@@ -6,10 +6,18 @@ public class InventoryClientStub {
 
     public static void stubInventoryCall(String skuCode, Integer quantity) {
 
-        stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("true")));
+        if (quantity <= 100) {
+            stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody("true")));
+        } else {
+            stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody("false")));
+        }
     }
 }
